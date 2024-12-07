950e3157ae85d78822c54edefdbab33e2935e146
bhanukiran1535 <bhanukiran1535@gmail.com>
2024-12-07 10:14:43 +0530

    Initial commit

--- site.js
+++ site.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const element= React.createElement("h1",{id: "itsh1", style: {}},"Namasthe React");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
+const elementt= React.createElement("h1",{id: "itsh12", style: {}},"Namasthe React");
+const roott=ReactDOM.createRoot(document.getElementById("roott"));
+roott.render(elementt);
